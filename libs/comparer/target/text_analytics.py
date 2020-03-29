2c58e4a54ffdc0358d4c354f4f2a786934512c50
Aunne <asd456ef@gmail.com>
2020-03-29 16:02:16 +0800

    o

--- libs/comparer/target/text_analytics.py
+++ libs/comparer/target/text_analytics.py
@@ -1,18 +1,14 @@
 import requests
 from bs4 import   BeautifulSoup
 
-url_origin="https://www.google.com"
+url_origin="https://account.google.com/signin/v2/indentfier?flowName=GlifWebSignIn&flowEntry=ServiceLogin"
 url_code=requests.get(url=url_origin).text
-soup=BeautifulSoup(url_code,'html.parser')
+soup=BeautifulSoup(url_code,'lxml')
 
-print(soup.b)
+
 
 
 
 """
-URL_code=requests.get('https://project.starinc.xyz/pbp_test/test.php').text
-BS_URL_cd=BeautifulSoup(URL_code,'html.parser')
-URL_Tag_text=BS_URL_cd.get_text()
-A=URL_Tag_text.replace("\n","").replace("\r","")
-print(A.text)
+https://project.starinc.xyz/pbp_test/test.php
 """
